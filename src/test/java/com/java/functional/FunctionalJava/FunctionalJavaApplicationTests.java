b484ce6ce8c6e4c1e89355c9a35459cf84ca85d9
Vivid-Vortex <ctntc9.dpk@gmail.com>
2025-03-17 14:02:45 +0530

    init

---
+++ src/test/java/com/java/functional/FunctionalJava/FunctionalJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.functional.FunctionalJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
